de4e29b63dfa0da287ee594e728ecc34b6818a39
Eric Sabelhaus <eric.sabelhaus@gmail.com>
2014-12-09 07:57:13 -0500

    condensed routes, poet now working

--- app.js
+++ app.js
@@ -1,81 +1,83 @@
 var express = require('express'),
     app = express(),
-    Poet = require('poet'),
     passport = require('passport'),
     path = require('path'),
+    Poet = require('poet'),
     favicon = require('serve-favicon'),
     logger = require('morgan'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
-    routes = require('./routes/index'),
-    users = require('./routes/users'),
-    login = require('./routes/login');
-
- var poet = Poet(app, {
-   posts: './_posts/',
-   postsPerPage: 5,
-   metaFormat: 'json'
- });
+    routes = require('./routes/index');
 
 (function(){
  "use strict";
 
- poet.init().then(function () {
-   // ready to go!
- });
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+  app.use(favicon(__dirname + '/public/favicon.ico'));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  app.use('/', routes);
+  app.use('/users', routes);
+  app.use('/login', routes);
+  app.use('/register', routes);
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/login', login);
+  var poet = new Poet(app, {
+    posts: './_posts/',
+    postsPerPage: 5,
+    metaFormat: 'json'
+  });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+  poet.init().then(function () {
+    // ready to go!
+  });
 
-// error handlers
+  app.set('view engine', 'jade');
+  app.set('views', __dirname + '/views');
+  app.use(express.static(__dirname + '/public'));
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+      var err = new Error('Not Found');
+      err.status = 404;
+      next(err);
+  });
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+  // error handlers
+
+  //development error handler
+  //will print stacktrace
+  if (app.get('env') === 'development') {
+      app.use(function(err, req, res, next) {
+          res.status(err.status || 500);
+          res.render('error', {
+              message: err.message,
+              error: err
+          });
+      });
+  }
+
+  //production error handler
+  //no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+          message: err.message,
+          error: {}
+      });
+  });
 
 
-app.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' }));
+  app.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' }));
 
-module.exports = app;
+  module.exports = app;
 
+  app.listen(3000);
 
-app.listen(3000);
 })();
--- routes/index.js
+++ routes/index.js
@@ -4,25 +4,29 @@ var router = express.Router();
 (function(){
   "use strict";
 
+    /* GET home page. */
+  router.get('/', function(req, res) {
+    res.render('index');
+  });
+
   /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index');
-});
+  router.get('/login', function(req, res) {
+    res.render('login');
+  });
 
-/* GET home page. */
-router.get('/login', function(req, res) {
-  res.render('login');
-});
+  /* GET home page. */
+  router.get('/register', function(req, res) {
+    res.render('register');
+  });
 
-/* GET home page. */
-router.get('/register', function(req, res) {
-  res.render('register');
-});
+  /* GET users listing. */
+  router.get('/users', function(req, res) {
+    res.render('user');
+  });
 
-/* GET users listing. */
-router.get('/users', function(req, res) {
-  res.render('user');
-});
+  router.get('/profile', function(req, res) {
+    res.render('profile');
+  });
 
 module.exports = router;
 })();
